6cf7e64ff1264b978e7dc1fa82f233f9b2bde09b
WpWebr <105921512+WpWebr@users.noreply.github.com>
2026-03-04 04:23:14 +0100

    config

--- template.config.js
+++ template.config.js
@@ -2,12 +2,12 @@
 const projectName = path.basename(path.resolve()).toLowerCase()
 
 export default {
-  lang: 'ua',
-  vscode: {
+  lang: 'ua', // язык сообщений
+  vscode: { // настройка редактора (не меняет настройки по умолчанию - только для этой сборки)
     settings: true,
     snippets: true
   },
-  navpanel: {
+  navpanel: { // навигация по страницам
     dev: true,
     build: false,
     position: 'left',
@@ -15,79 +15,79 @@ export default {
     background: 'rgba(51, 51, 51, 0.5)',
     transition: '300'
   },
-  server: {
-    path: './',
-    copyfiles: true,
-    version: true,
+  server: { // сервер
+    path: './', // путь
+    copyfiles: true, // сопии файлов с files
+    version: true, // версия файлов
     hostname: 'localhost',
     port: '1111'
   },
   html: {
-    beautify: {
+    beautify: { // форматирование
       enable: true,
       indent: "tab"
     }
   },
   styles: {
-    tailwindcss: false,
-    pxtorem: true,
-    critical: false,
-    codesplit: true,
-    devfiles: true,
+    tailwindcss: false, // вкл.tailwind(стандартный reset отключается)
+    pxtorem: true, // перевод пикселей (px) в rem при сборке
+    critical: false, // в проде стили видимой части экрана доюавляются в html
+    codesplit: true, // розбивание файлов по страницам ( https://vite.dev/ )
+    devfiles: true, // + не сжатые файлы
   },
-  fonts: {
-    iconsfont: false,
-    download: false
+  fonts: { // шрифт
+    iconsfont: false, // создание иконочного шрифта (src\assets\svgicons)
+    download: false // загрузка шрифтов в локальную папку при проде
   },
   images: {
-    svgsprite: true,
-    optimize: {
-      enable: true,
+    svgsprite: true, // создание спрайта
+    optimize: { // оптимизация изображений
+      enable: true, // вкл
       edithtml: true,
-      sizes: [600, 1200],
-      dpi: [],
-      attrignore: 'data-fls-image-ignore',
+      sizes: [600, 1200], // размеры изображений которые нужно создать
+      dpi: [], // множитель для Retina (x2,x3 - [2,3])
+      attrignore: 'data-fls-image-ignore', // еси добавлен атрибут, оптимизации нет
       modernformat: {
-        enable: true,
+        enable: true, // использовать webp/avif
         type: 'webp', // webp/avif
-        only: true,
+        only: true, // удаление исходника
+        quality: 80 // качество изображения
+      },
+      jpeg: { // если modernformat.enable.false
         quality: 80
       },
-      jpeg: {
-        quality: 80
-      },
-      png: {
+      png: { // если modernformat.enable.false
         quality: 80
       }
     }
   },
   js: {
-    hotmodules: true,
-    devfiles: true,
+    hotmodules: true, // вкл. компонентов. Напр.: снипет tab и автоматом подключает js,css (src\components\layout\tabs)
+    devfiles: true, // копии не сжатых js-файлов
     bundle: {
       // Собирает в один JS и один CSS файлы
       // независимо от настройки
       // styles -> codesplit,
       enable: false,
     },
-    react: false,
-    vue: false
+    react: false, // подкл. React
+    vue: false // подкл. Vue
   },
-  php: {
+  php: { // локальный сервер PHP
     enable: false,
     base: "./src/php/",
     hostname: 'localhost',
     port: '1110',
-    binary: 'C:\\php\\php.exe',
+    binary: 'C:\\php\\php.exe', // путь к php
     ini: 'template_modules/assets/php.ini',
   },
-  pug: {
+  pug: { // src\pug
     enable: false,
   },
-  ftp: {
+  ftp: { // прод на сервер
     host: '127.0.0.1',
     port: 21,
-    remoteDir: `/www/.../${projectName}`,
+    remoteDir: `/www/.../${projectName}`, // projectName -имя проекта
     user: 'root',
     password: '123456',
   },
@@ -97,10 +97,10 @@ export default {
     // Логи работы модулей в консоли
     console: {
       enable: true,
-      removeonbuild: true
+      removeonbuild: true // удаляется в прод
     }
   },
-  projectpage: {
+  projectpage: { // Шаблог страницы проэкта
     enable: false,
     projectname: '',
     template: "src/components/templates/projectpage/projectpage.html",
@@ -126,7 +126,7 @@ export default {
     text: `(!!)Хватит работать, сделай перерыв ☕️`,
     interval: 45
   },
-  novaposhta: {
+  novaposhta: { // Новая почта
     enable: false,
     key: ''
   }
