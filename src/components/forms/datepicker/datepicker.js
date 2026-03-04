17e75bc3016f6dd9c9d5b3da1320033b62fe0f5a
WpWebr <105921512+WpWebr@users.noreply.github.com>
2026-03-04 04:50:31 +0100

    datepicker.js

--- src/components/forms/datepicker/datepicker.js
+++ src/components/forms/datepicker/datepicker.js
@@ -1,22 +1,24 @@
-// Підключення модуля
+// Подключение модуля
 import datepicker from 'js-datepicker';
 
 import './datepicker.scss'
 
 if (document.querySelector('[data-fls-datepicker]')) {
-	const picker = datepicker('[data-fls-datepicker]', {
-		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
-		customMonths: ["Січ", "Лют", "Берез", "Квіт", "Трав", "Черв", "Лип", "Серп", "Верес", "Жовт", "Листоп", "Груд"],
-		overlayButton: 'Застосувати',
-		overlayPlaceholder: 'Рік (4 цифри)',
-		startDay: 1,
-		formatter: (input, date, instance) => {
-			const value = date.toLocaleDateString()
-			input.value = value
-		},
-		onSelect: function (input, instance, date) {
+  const picker = datepicker('[data-fls-datepicker]', {
+    // customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
+    customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+    // customMonths: ["Січ", "Лют", "Берез", "Квіт", "Трав", "Черв", "Лип", "Серп", "Верес", "Жовт", "Листоп", "Груд"],
+    customMonths: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Ноябрь", "Дек"],
+    overlayButton: 'Применить',
+    overlayPlaceholder: 'Год (4 цифры)',
+    startDay: 1,
+    formatter: (input, date, instance) => {
+      const value = date.toLocaleDateString()
+      input.value = value
+    },
+    onSelect: function (input, instance, date) {
 
-		}
-	});
-	flsModules.datepicker = picker;
+    }
+  });
+  flsModules.datepicker = picker;
 }
